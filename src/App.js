91aa37a2344b612ec00f53c748dec2c0cd5e8042
vihith arekatla <arekatlavihith@gmail.com>
2023-02-01 23:23:08 +0530

    created login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import Form from "./Components/Form";
+import Signup from "./Components/Signup"
+import Login from "./Components/Login"
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 function App() {
   return (
-    <Form />
+    <Router>
+      <Routes>
+      <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/signup" element={<Signup />}></Route>
+        <Route exact path="/" element={<Form />} ></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Signup.js
@@ -0,0 +1,26 @@
+import {Link} from "react-router-dom";
+export default function Signup(){
+    return(
+        <div class="w-screen h-screen p-2 bg-[#202543]">
+            <div class="w-6/12 justify-evenly p-2 rounded-lg flex flex-col h-4/5 mx-auto mt20 bg-[#2D335B]">
+                <div class="text-center text-4xl text-white"><span className="text-[#469DFF]">S</span>ocial <span className="text-[#469DFF]">A</span>nlytics</div>
+                <div>
+                    <div class="mx-auto flex flex-col w-7/12">
+                        <input type="text" autoComplete="off" className="my-2 text-white outline-none p-2 text-md bg-[#363E70]" id="usn" placeholder="enter username" />
+                        <input type="password" autoComplete="off" className="my-2 text-white outline-none p-2 text-md bg-[#363E70]" id="pass" placeholder="enter password" />
+                    </div>
+                </div>
+                <div className="text-center">
+                    <button className="p-1 hover:bg-[#469DFF] text-white border-2 border-[#469DFF] rounded text-xl text-center px-3 m-2">SignUp</button>
+                </div>
+                <div className="text-center opacity-60 hover:opacity-100 text-[#469DFF]">
+                    <Link to="/login">Go To Login</Link>
+                </div>
+                <div className="text-center invisible text-xl text-white">
+                    Username already exists
+                </div>
+            </div>
+
+        </div>
+    );
+}
